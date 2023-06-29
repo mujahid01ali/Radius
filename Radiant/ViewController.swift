38133a362fc1b8b55bdca0d5d3be59696b51628b
Mujahid <mujahid@classplus.co>
2023-06-30 00:51:04 +0400

    Initial Commit

---
+++ Radiant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Radiant
+//
+//  Created by Mujahid Ali on 30/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
